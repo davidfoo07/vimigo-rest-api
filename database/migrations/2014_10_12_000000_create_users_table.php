36543646fb8e3ade271e637d24011c5ace9d2fd6
davidfoo07 <beckhamfth@gmail.com>
2024-02-27 02:57:27 +0800

    vimigo-rest-api

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('address');
-            $table->string('study_course');
             $table->rememberToken();
             $table->timestamps();
         });
